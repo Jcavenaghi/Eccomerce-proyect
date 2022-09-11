38cede1a2f86d70186d7368f54900058b82482da
Joaquin Cavenaghi <joaquincave@outlook.com>
2022-09-11 16:16:27 -0300

    producto y calculo. poco de css

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
 
 //imports
 // import Producto from "./producto.js"
-class Producto {
-    constructor(nombre, codigo,  precio) {
-        this.nombre = nombre;
+class Celular {
+    constructor(modelo, codigo,  precio) {
+        this.modelo = modelo;
         this.codigo = codigo;
         this.precio = precio;
     }
 
+    getPrecio() {
+        return this.precio * 1.21;
+    }
 }
 
 //variables
-let productos = []
+let celulares = []
+
+celulares.push(new Celular('iPhone 14', '1', 1100))
+celulares.push(new Celular('iPhone 13', '2', 899))
+celulares.push(new Celular('iPhone 12 Pro Max', '3', 1050))
+celulares.push(new Celular('iPhone 12', '4', 800))
+celulares.push(new Celular('iPhone 11', '5', 747))
+celulares.push(new Celular('iPhone XS', '6', 700))
+
 let iva21 = 314.14
 
 //funciones
@@ -21,17 +32,14 @@ function aleatorio(minimo,maximo){
 
 function calcular_total() {
     total = 0.0;
-    productos.forEach(prod => {
-        total+= prod["precio"]
+    celulares.forEach(cel => {
+        precioConIva = cel.getPrecio()
+        alert(cel["modelo"] + ", precio con iva: " + precioConIva);
+        total+= precioConIva;
     });
-
-    alert("el total es: " + (total + iva21) + " Dolares") // se aplica el iva al precio final, no al de cada producto.
+    alert("el valor de todos los iphone en stock es de: " + (total) + " Dolares") // se aplica el iva al precio final, no al de cada producto.
 }
 
 
 //codigo js
 
-for (let i = 0; i < 5; i++) {
-    producto  = new Producto ('Iphone' + i, "Iphone: AAA" + i, aleatorio(100, 100000));
-    productos.push(producto);
-}
